7f15201ed4edabe59b9291154599c0a5cfe55802
hangju0610 <phj950610@naver.com>
2023-09-07 20:15:05 +0900

    Feat : validation, Transform 활용

--- src/video/dto/req.dto.ts
+++ src/video/dto/req.dto.ts
@@ -1,7 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUUID, MaxLength, MinLength } from 'class-validator';
 
 export class CreateVideoReqDto {
   @ApiProperty({ required: true })
+  @MinLength(2)
+  @MaxLength(30)
+  @IsString()
   title: string;
 
   // 실제 비디오 영상을 바이너리 형태로 업로드
@@ -11,5 +15,6 @@ export class CreateVideoReqDto {
 
 export class FindVideoReqDto {
   @ApiProperty({ required: true })
+  @IsUUID()
   id: string;
 }
