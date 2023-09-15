78db1274c1c3eead5af38c8cbbff0b5542acfadf
hangju0610 <phj950610@naver.com>
2023-09-15 22:28:10 +0900

    Feat : useValue를 이용한 CustomProvider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,25 @@ import { User } from './entity/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
+// Mock 함수 제작
+const UserMockService = {
+  findAll: () => {
+    return 'find mock users';
+  },
+};
+
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [UserService],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    // UserService,
+    {
+      // provide 이름은 UserService
+      provide: UserService,
+      // 실제 제공하는 useValue 값은 UserMockService
+      useValue: UserMockService,
+    },
+  ],
 })
 export class UserModule {}
