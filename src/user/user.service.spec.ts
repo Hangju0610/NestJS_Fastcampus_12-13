e7d3978009f490bf8ae08c57585035560119bd7c
hangju0610 <phj950610@naver.com>
2023-09-15 22:40:44 +0900

    Feat: useClass를 이용하여 Jest 단위 test Code 작성

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+
+class MockRepository {
+  async findOneBy(query) {
+    const user: User = new User();
+    user.email = query.email;
+    return user;
+  }
+}
+
+describe('User', () => {
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockRepository,
+        },
+      ],
+    }).compile();
+    userService = module.get<UserService>(UserService);
+  });
+
+  it('should', async () => {
+    const email = 'nestjs@gmail.com';
+    const result = await userService.findOneByEmail(email);
+    expect(result.email).toBe(email);
+  });
+});
